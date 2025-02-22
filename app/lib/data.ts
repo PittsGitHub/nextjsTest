d9f0a5523cd3c06e803736e0e0fd1aecb6e680c9
Daniel Pittard <126673498+PittsGitHub@users.noreply.github.com>
2025-02-22 19:23:01 +0000

    Dynamic Loading latest invoices

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -16,6 +16,11 @@ import { formatCurrency } from "./utils";
 
 export async function fetchRevenue() {
   try {
+    // We artificially delay a response for demo purposes.
+    // Don't do this in production :)
+    console.log("Fetching revenue data...");
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+
     const data = await sql<Revenue[]>`SELECT * FROM "Revenue"`;
     return data;
   } catch (error) {
