f9d621ebc0d711440a9d072a01e3dc1ad6f5d8cf
Daniel Pittard <126673498+PittsGitHub@users.noreply.github.com>
2025-02-22 19:38:23 +0000

    dynamic loading of card collection

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,4 +1,4 @@
-import { Card } from "@/app/ui/dashboard/cards";
+import CardWrapper, { Card } from "@/app/ui/dashboard/cards";
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import { lusitana } from "@/app/ui/fonts";
@@ -18,32 +18,14 @@ export default async function Page() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <Card
-          title="Collected"
-          value={cardData.numberOfInvoices}
-          type="collected"
-        />
-        <Card
-          title="Pending"
-          value={cardData.totalPendingInvoices}
-          type="pending"
-        />
-        <Card
-          title="Total Invoices"
-          value={cardData.totalPaidInvoices}
-          type="invoices"
-        />
-        <Card
-          title="Total Customers"
-          value={cardData.numberOfCustomers}
-          type="customers"
-        />
+        <Suspense fallback={<CardSkeleton />}>
+          <CardWrapper />
+        </Suspense>
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <Suspense fallback={<RevenueChartSkeleton />}>
           <RevenueChart />
         </Suspense>
-
         <Suspense fallback={<LatestInvoicesSkeleton />}>
           <LatestInvoices />
         </Suspense>
